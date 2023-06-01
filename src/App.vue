690b0ad93e89e1484698f769b9a93383f92be610
jacom3dev <josejuliandlf@gmail.com>
2023-05-31 23:56:59 -0500

    change basic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/layout/Header.vue';
+    import Footer from './components/layout/Footer.vue';
+    import Guitar from './components/Guitar.vue';
+
+    import {ref,onMounted} from "vue";
+    import {db} from "./data/guitars";
+
+    const guitars = ref([]);
+
+    onMounted(()=>{
+        guitars.value = db;
+    })
 </script>
 
 <template>
  
-  <HelloWorld msg="Vite + Vue" />
+  <Header/>
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+    <div class="row mt-5">
+        <Guitar
+            v-for="guitar in guitars"
+           :guitar="guitar"
+        />
+    </div>
+  </main>
+  <Footer/>
+
+  
 </template>
 
 <style scoped>
